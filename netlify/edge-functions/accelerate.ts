e39a145bd032aa7a9c3a99302bcd2d612f31d6e5
Luan van der Westhuizen <luan.vdw@gmail.com>
2024-03-12 13:30:40 +0200

    feat: add Prisma

--- netlify/edge-functions/accelerate.ts
+++ netlify/edge-functions/accelerate.ts
@@ -1,9 +1,19 @@
 import type { Config, Context } from "@netlify/edge-functions";
-
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 export default async (request: Request, context: Context) => {
+
+  const dbURL = Netlify.env.get("MY_IMPORTANT_VARIABLE")
+
+  const prisma = new PrismaClient({
+    datasourceUrl: dbURL,
+  }).$extends(withAccelerate())
+
+  const users = await prisma.user.findMany();
+
   return Response.json({
-    geo: context.geo,
+    users: users,
   });
 };
 
